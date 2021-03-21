0bbc720cc986fa1fe40a3ac3762f7ae53ea02ef5
kierentm <kierentm@gmail.com>
2021-03-21 15:44:02 +0000

    Main Changes
    Added the 5 boxes in the grid.
    
    Added an item name that can be used for the exported txt name + "colour info".
    
    Added a drop down box but it currently uses a pack for formatting instead of a grid so its awkwardly at the bottom of the screen.

--- main.py
+++ main.py
@@ -12,37 +12,68 @@ def rbg_2_hex():
 
 
 window = tk.Tk()
-window.title("Colour Picker")
-window.geometry('300x500')
+window.title("Colour Tint Converter")
+window.geometry('350x400')
 
-tab_control = ttk.Notebook(window)
-
-main = ttk.Frame(tab_control)
+tab_control = ttk.Notebook()
+main = ttk.Frame(tab_control, borderwidth=5)
+main_content_1 = ttk.Frame(main, borderwidth=10, relief="groove")
+main_content_2 = ttk.Frame(main, borderwidth=10, relief="groove")
 settings = ttk.Frame(tab_control)
-hotkeys = ttk.Frame(tab_control)
+
 about = ttk.Frame(tab_control)
 
 tab_control.add(main, text='Main')
+main_content_1.grid(column=1, row=2)
+main_content_2.grid(column=1, row=0)
+
 tab_control.add(settings, text='Settings')
-tab_control.add(hotkeys, text='Hotkeys')
+
 tab_control.add(about, text='About')
 
 tab_control.pack(expand=1, fill='both')
 
-rbg_label = tk.Label(main, text="RGB", font=("Arial Bold", 10))
-rbg_label.grid(column=0, row=1)
+#Testing Item Name
+ItemNameLabel = tk.Label(main_content_2, text="ItemName")
+ItemNameLabel.grid(column=0,row=2)
+Item_Name_Entry = tk.Entry(main_content_2, width =24)
+Item_Name_Entry.grid(column=1, row=2)
 
-R_spin = tk.Spinbox(main, from_=0, to=255, width=5)
-R_spin.grid(column=0, row=2)
+#name field
 
-G_spin = tk.Spinbox(main, from_=0, to=255, width=5)
-G_spin.grid(column=1, row=2)
+Colour_Tint_Name = tk.Entry(main_content_1, width =24)
+Colour_Tint_Name.grid(column=1, row=2)
 
-B_spin = tk.Spinbox(main, from_=0, to=255, width=5)
-B_spin.grid(column=2, row=2)
+#Red green and blue field
 
-rgb_convert = tk.Button(main, text="Convert", command=rbg_2_hex)
-rgb_convert.grid(column=3, row=2)
+R_spin = tk.Entry(main_content_1, width =4)
+R_spin.grid(column=2, row=2)
+
+
+G_spin = tk.Entry(main_content_1, width =4)
+G_spin.grid(column=3, row=2)
+
+
+B_spin = tk.Entry(main_content_1, width =4)
+B_spin.grid(column=4, row=2)
+
+#auto convert hex field
+
+Hex_spin = tk.Entry(main_content_1, width =10)
+Hex_spin.grid(column=5, row=2)
+
+#drop down box
+
+Drop_Down = ["Mult","Add"]
+Drop_Down_Variable = tk.StringVar(window)
+Drop_Down_Variable.set(Drop_Down[0])
+
+opt = tk.OptionMenu(window, Drop_Down_Variable, *Drop_Down)
+opt.pack()
+
+#rgb_convert = tk.Button(main, text="Convert", command=rbg_2_hex)
+#rgb_convert.grid(column=4, row=2)
+
 
 window.wm_attributes("-topmost", 1)
 window.mainloop()
