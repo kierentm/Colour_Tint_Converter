6589f1535dec1a0f6ac720aeb65489bd812550a9
alextodd434 <alextodd434@gmail.com>
2021-03-24 23:18:11 +0000

    FixedNonLinearToLinear

--- convert.py
+++ convert.py
@@ -1,1 +1,7 @@
 # Python Module to convert to sRGB
+def linearsrgbtolinear(rnl):
+    if rnl < 0.0031308:
+        rl = rnl * 12.92
+    else:
+        rl = 1.055 * rnl ** (1 / 2.4) - 0.055
+    return rl
