11bd68afb0c36e244b1006886f2aecd38edcbfb9
JakeBroughton <46027440+JakeBroughton@users.noreply.github.com>
2021-03-25 00:01:08 +0000

    Added instances tracking to practice.py

--- practice.py
+++ practice.py
@@ -1,6 +1,5 @@
 import tkinter as tk
 from tkinter import ttk
-import convert
 
 root = tk.Tk()
 root.geometry("400x400")
@@ -29,17 +28,17 @@ class RemovableTint(tk.Frame):
     instances = []
 
     def __init__(self, parent_frame):
+        RemovableTint.instances.append(self)
+
         tk.Frame.__init__(self, parent_frame, height=5, pady=1)
         self.hex_entry_var = tk.StringVar()
-
         self.colour_tint_name = tk.Entry(self, width=15, font="Calibri")
         self.r_spin = tk.Entry(self, width=4, font="Calibri")
         self.g_spin = tk.Entry(self, width=4, font="Calibri")
         self.b_spin = tk.Entry(self, width=4, font="Calibri")
         self.hex_spin = tk.Entry(self, width=10, font="Calibri", textvariable=self.hex_entry_var)
         self.update = tk.Button(self, text="Update", font="Calibri", command=self.hex_conversion)
-        self.remove = tk.Button(self, font="Calibri",
-                                text="X", command=self.destroy)
+        self.remove = tk.Button(self, font="Calibri", text="X", command=self.remove)
 
         self.colour_tint_name.pack(fill="both", side="left", expand=True)
         self.r_spin.pack(fill="both", side="left")
@@ -50,17 +49,23 @@ class RemovableTint(tk.Frame):
         self.remove.pack(fill="both", side="left")
 
     def hex_conversion(self):
-        # hex_value = convert.linearsrgbtolinear()
+        # TODO: Combine with Alex's function
         hex_value = "DUMMYFFFFFFF"
         self.hex_entry_var.set(hex_value)
 
+    def remove(self):
+        RemovableTint.instances.remove(self)
+        self.destroy()
+        print(self.instances)
 
+
+# Add frame instance (dynamic addition of widgets)
 def add_frame():
     RemovableTint(home_frame).pack(fill=tk.X)
+    print(RemovableTint.instances)
 
 
 btn = tk.Button(home_frame, text="Add Entry", width=50, command=add_frame)
 btn.pack(side="bottom", fill=tk.X)
-home_frame.rowconfigure(0, weight=1)
-home_frame.columnconfigure(0, weight=1)
+
 home_frame.mainloop()
