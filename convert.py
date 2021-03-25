ed53225c538d4062c850ba4d584d5a2769a06abb
alextodd434 <alextodd434@gmail.com>
2021-03-25 00:46:57 +0000

    "Correctly" got hex value outputting

--- convert.py
+++ convert.py
@@ -1,13 +1,15 @@
 # Python Module to convert to sRGB
 
 # Function to convert non linear-srgb value to linear srb
-def linearsrgbtolinear(nlsrgb):
+def nonlinearsrgbtolinear(nlsrgb):
     lsrgb = []
     for nlvalue in nlsrgb:
         if nlvalue < 0.0031308:
             lvalue = nlvalue * 12.92
-            lsrgb.append(lvalue)
+            value8 = int(255 * lvalue)
+            lsrgb.append(value8)
         else:
             lvalue = 1.055 * nlvalue ** (1 / 2.4) - 0.055
-            lsrgb.append(lvalue)
-    return lsrgb
+            value8 = int(255 * lvalue)
+            lsrgb.append(value8)
+    return tuple(lsrgb)
