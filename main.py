93f39b5ddd92612003e9ffd9ce9166d129c107ee
alextodd434 <alextodd434@gmail.com>
2021-04-01 01:22:52 +0100

    TODO Add

--- main.py
+++ main.py
@@ -32,6 +32,7 @@ import pathlib
 
 # TODO: Create readme and video
 
+# TODO: Refactor meme function names
 # TODO: Remove debugging prints
 # TODO: Refactor some names (eg. spin)
 # TODO: Remove redundant self. tags (High Risk)
